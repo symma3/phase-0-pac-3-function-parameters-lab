a5f130ea26d9ed355cedaf2b451e347211f5677f
craig johnson <cj.craigajohnson@gmail.com>
2023-05-16 21:40:56 -0400

    completed test

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name) {
+ console.log(name)
+   return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name, language) {
+   console.log(`${name} ${language}`);
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language = Javascript) {
+   console.log(`${name} ${language}`);
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language = `JavaScript`) {
+   console.log(`${name} ${language}`);
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
